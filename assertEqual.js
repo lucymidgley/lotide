f9c8f52ef106ded9090f3be33dd04c0e2a6cde4a
lucymidgley <lucymidgley13@gmail.com>
2020-01-06 23:15:31 +0000

    Created a function to compare the two values it takes in and print out a message telling us if they match or not

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅Assertion Passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log("❌❌❌Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+//test code
+assertEqual("Lighthouse labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Hello", "Hello");
+assertEqual("Hello", "How are you");
+assertEqual(55, 55);
+assertEqual(1, -1);
