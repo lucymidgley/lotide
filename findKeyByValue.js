1338aa0eef5661778321d24c3f82c408b3528284
Lucy <lucymidgley13@gmail.com>
2020-01-08 21:08:50 +0000

    Created the function findKeyByValue which takes in an object and a value. It scans the object and return the first key which contains the given value. If no key with that given value is found, then it returns undefined.

---
+++ findKeyByValue.js
@@ -0,0 +1,53 @@
+//TEST ASSERTION FUNCTIONS
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//ACTUAL FUNCTION
+const findKeyByValue = function(object, value) {
+  let key;
+  for (const objectKey in object) {
+    if (object[objectKey] === value) {
+      key = objectKey;
+    }
+  }
+  return key;
+};
+
+//SOME TESTS
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+const bestMoviesShowsByGenre = {
+  sciFi: "Movie A",
+  comedy: "Movie B",
+  drama:  "Movie C"
+};
+
+assertEqual(findKeyByValue(bestMoviesShowsByGenre, "Movie A"), "sciFi");
+assertEqual(findKeyByValue(bestMoviesShowsByGenre, "Movie B"), "comedy");
+assertEqual(findKeyByValue(bestMoviesShowsByGenre, "Movie C"), "drama");
+assertEqual(findKeyByValue(bestMoviesShowsByGenre, "Movie D"), undefined);
+
+const person = {
+  name: "Lucy",
+  age: 25,
+  hobby:  "trumpet",
+  transport: "bicyle",
+  country: "England"
+};
+
+assertEqual(findKeyByValue(person, "Lucy"), "name");
+assertEqual(findKeyByValue(person, 25), "age");
+assertEqual(findKeyByValue(person, "bicyle"), "transport");
+assertEqual(findKeyByValue(person, "Patrick"), undefined);
