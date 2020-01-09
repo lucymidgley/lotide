a9b3a14acf02893f608455a54f584ea3aee15fd7
Lucy <lucymidgley13@gmail.com>
2020-01-09 22:13:29 +0000

    Created a function findKey that takes in an object and a callback Fucntion and returns the first key of the object where the callback evaluates to true

---
+++ findKey.js
@@ -0,0 +1,56 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//FUNCTION
+
+const findKey = function(object, callback) {
+  for (const objectKey in object) {
+    if (callback(object[objectKey])) {
+      return objectKey;
+    }
+  }
+};
+
+
+
+const restaurants = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  sameDrama: "The Wire"
+};
+const planets = {
+  "Mercury": { moons: 0 },
+  "Venus":   { moons: 0 },
+  "Earth":   { moons: 1 },
+  "Mars":    { moons: 2 },
+  "Jupier":  { moons: 79 },
+  "Saturn":  { moons: 82 },
+  "Uranus":  { moons: 27 },
+  "Neptune": { moons: 14 }
+};
+
+assertEqual((findKey(restaurants, x => x.stars === 2)), "noma");
+assertEqual((findKey(bestTVShowsByGenre, x => x === "The Wire")), "drama");
+assertEqual((findKey(planets, x => x.moons === 0)), "Mercury");
+
+// const isTheWire = function(x){
+//   return x === "The Wire"
+// }
+
+// const is2 = function(x) {
+//   return x.stars === 2
+// }
