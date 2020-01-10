79c8c79b1c36791deac55fa72d686d1ece83ab80
Lucy <lucymidgley13@gmail.com>
2020-01-10 23:01:21 +0000

    modified the function to allow for nested objects using recusion

--- eqObjects.js
+++ eqObjects.js
@@ -25,10 +25,13 @@ const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false; //checked both objects have same length of Object keys array
   } // this means we only need to check one set of keys, if they match then they are the same!
-  for (const objectKey of Object.keys(object1)) { //first check if both valjues are arrays
-    if (Array.isArray(object1[objectKey]) && Array.isArray(object2[objectKey])) {
+  for (const objectKey of Object.keys(object1)) { //first check if both values are objects but not arrays
+    if (!Array.isArray(object1[objectKey]) && !Array.isArray(object2[objectKey]) && object1[objectKey].constructor === Object && object2[objectKey].constructor === Object) {
+      return eqObjects(object1[objectKey], object2[objectKey]);
+    }
+    if (Array.isArray(object1[objectKey]) && Array.isArray(object2[objectKey])) { //then check if both values are arrays
       if (!eqArrays(object1[objectKey], object2[objectKey])) {
-        return false; //if so check if both arrays are same
+        return false; //if so, check if both arrays are same
       }
     } else if (object1[objectKey] !== object2[objectKey]) { //else assume both are primitive
       return false; //check they are same
@@ -56,3 +59,7 @@ assertEqual(eqObjects(cd3, ab), false);
 
 const dc3 = { e: [1,2,3], d: ["2", 3] ,  c: "1" };
 assertEqual(eqObjects(cd3, dc3), true);
+/// now with nested objects
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
