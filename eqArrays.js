2cad413509b885e353fc080cf87696660655801f
Lucy <lucymidgley13@gmail.com>
2020-01-10 23:40:45 +0000

    modified eqArrays to allow nested arrays using recusion. Fixed small error in eqObjects

--- eqArrays.js
+++ eqArrays.js
@@ -6,20 +6,32 @@ const assertEqual = function(actual, expected) {
   }
 };
 const eqArrays = function(arr1, arr2) {
-  let trueFalse = true;
   if (arr1.length !== arr2.length) {
-    trueFalse = false;
-  } else {
-    for (let i = 0; i < arr2.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        trueFalse = false;
+    return false;
+  } for (let i = 0; i < arr2.length; i++) {
+      if(Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+       if(!eqArrays(arr1[i], arr2[i])){
+         return false
+       }
+       }
+       else if (arr1[i] !== arr2[i]) {
+        return false;
       }
     }
-  }
-  return trueFalse;
+  return true;
 };
 
 assertEqual(eqArrays([1, 2], [1, 2, 3]), false); // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
+console.log("nested arrays tests")
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true) // => true
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false) // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false)
+assertEqual(eqArrays([[2, 3], [4,[5, 6]]], [[2, 3], 4]), false)
+assertEqual(eqArrays([[2, 3], [4,[5, 6]]], [[2, 3], [4,[5, 6]]]), true)
+assertEqual(eqArrays([[2, 3], [4], 5, 6], [[2, 3], 4]), false)
+assertEqual(eqArrays([[2, 3], [4], 5, 6], [[2, 3], [4,[5, 6]]]), false)
+ // => false
--- eqObjects.js
+++ eqObjects.js
@@ -27,9 +27,10 @@ const eqObjects = function(object1, object2) {
   } // this means we only need to check one set of keys, if they match then they are the same!
   for (const objectKey of Object.keys(object1)) { //first check if both values are objects but not arrays
     if (!Array.isArray(object1[objectKey]) && !Array.isArray(object2[objectKey]) && object1[objectKey].constructor === Object && object2[objectKey].constructor === Object) {
-      return eqObjects(object1[objectKey], object2[objectKey]);
-    }
-    if (Array.isArray(object1[objectKey]) && Array.isArray(object2[objectKey])) { //then check if both values are arrays
+      if(!eqObjects(object1[objectKey], object2[objectKey])){
+        return false
+      };
+    } else if (Array.isArray(object1[objectKey]) && Array.isArray(object2[objectKey])) { //then check if both values are arrays
       if (!eqArrays(object1[objectKey], object2[objectKey])) {
         return false; //if so, check if both arrays are same
       }
