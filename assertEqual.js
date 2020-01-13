3f606c4735219119aa52b4f825daa8a64ed497e0
Lucy <lucymidgley13@gmail.com>
2020-01-13 16:26:00 +0000

    Refactored code using require to import functions and made separate subfolder to test the code

--- assertEqual.js
+++ assertEqual.js
@@ -6,10 +6,6 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-//test code
-assertEqual("Lighthouse labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Hello", "Hello");
-assertEqual("Hello", "How are you");
-assertEqual(55, 55);
-assertEqual(1, -1);
+
+
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual');
+
+//test code
+assertEqual("Lighthouse labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Hello", "Hello");
+assertEqual("Hello", "How are you");
+assertEqual(55, 55);
+assertEqual(1, -1);
