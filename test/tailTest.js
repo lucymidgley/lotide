1596347bee48e305cd130474218c8f11a593104f
Lucy <lucymidgley13@gmail.com>
2020-01-13 16:37:19 +0000

    Refactored using require to import test functions and to test the function tail in a separate subfolder

---
+++ test/tailTest.js
@@ -0,0 +1,11 @@
+const tail = require('../tail');
+const assertEqual = require('../assertEqual');
+
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+assertEqual(words.length, 3);
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
